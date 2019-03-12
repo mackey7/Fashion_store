8dc47fa90140fdde0fe72a0238db9a79bc5c6a52
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-12 22:38:19 +0100

    add rwd for menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -24,27 +24,27 @@ export default class Navigation extends Component {
                     <li><a href="">men </a></li>
                     <li><a href="">clearence</a></li>
                 </ul>
-                <div>
-                    <div class="bar-cart-container">
-                        <div onClick={this.handleClick} className="bar-cart">
-                            <span>Your cart</span>
-                            <i class="fas fa-sort-down"></i>
+
+                <div class="bar-cart-container">
+                    <div onClick={this.handleClick} className="bar-cart">
+                        <span>Your cart</span>
+                        <i class="fas fa-sort-down"></i>
+                    </div>
+
+                    <div className="bar-expense">
+                        <i class="fas fa-shopping-bag"></i>
+                        <div>
+                            <span>0 ITEMS</span>
+                            <span>$0</span>
                         </div>
-
-                        <div className="bar-expense">
-                            <i class="fas fa-shopping-bag"></i>
-                            <div>
-                                <span>0 ITEMS</span>
-                                <span>$0</span>
-                            </div>
-
-                        </div>
-                        {this.state.cartDetails ?
-                            <CartDetails /> : null}
-
 
                     </div>
+                    {this.state.cartDetails ?
+                        <CartDetails /> : null}
+
+
                 </div>
+
             </nav >
         )
     }

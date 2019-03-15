c849dedd1cf4d6af7ae6244c905199050cdfa085
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-15 11:20:25 +0100

    change import date

--- src/App.js
+++ src/App.js
@@ -7,27 +7,10 @@ import Slider from "./components/Slider/Slider.jsx";
 import Products from "./components/Products/Products.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import { addToCart } from "./actions/actions";
-
+import { carouselSlidesData } from './api/carouselSlidesData.js'
 import "./App.css";
 
-const carouselSlidesData = [
-  {
-    img:
-      "http://keituhaircare.com/wp-content/uploads/2015/12/Home-Shop-Slider-4-1.jpg",
-    alt: "zdjęcie"
-  },
-  {
-    img:
-      "http://myshoproller.com/media/thememegastore/general/megastore_slider5.jpg",
-    alt: "zdjęcie"
-  },
-  {
-    img:
-      "http://www.settotal.com/wp-content/uploads/2016/11/slider1-e-shop-wp.jpg",
-    alt: "zdjęcie"
-  },
 
-];
 
 class App extends Component {
   handleClick = id => {
---
+++ src/api/carouselSlidesData.js
@@ -0,0 +1,18 @@
+export const carouselSlidesData = [
+    {
+        img:
+            "http://keituhaircare.com/wp-content/uploads/2015/12/Home-Shop-Slider-4-1.jpg",
+        alt: "zdjęcie"
+    },
+    {
+        img:
+            "http://myshoproller.com/media/thememegastore/general/megastore_slider5.jpg",
+        alt: "zdjęcie"
+    },
+    {
+        img:
+            "http://www.settotal.com/wp-content/uploads/2016/11/slider1-e-shop-wp.jpg",
+        alt: "zdjęcie"
+    },
+
+];

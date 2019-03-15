44d0a898ce078eb09e59bd481eee3c7fe0646841
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-15 11:04:45 +0100

    change  images in slider

--- src/App.js
+++ src/App.js
@@ -13,14 +13,20 @@ import "./App.css";
 const carouselSlidesData = [
   {
     img:
-      "http://zow.lublin.eu/wp-content/themes/the-wp/images/slider/slider4.jpg",
+      "http://keituhaircare.com/wp-content/uploads/2015/12/Home-Shop-Slider-4-1.jpg",
     alt: "zdjęcie"
   },
   {
     img:
-      "https://vignette.wikia.nocookie.net/thefutureofeuropes/images/d/d7/Adolf-hitler-620-638970380.jpg",
+      "http://myshoproller.com/media/thememegastore/general/megastore_slider5.jpg",
     alt: "zdjęcie"
-  }
+  },
+  {
+    img:
+      "http://www.settotal.com/wp-content/uploads/2016/11/slider1-e-shop-wp.jpg",
+    alt: "zdjęcie"
+  },
+
 ];
 
 class App extends Component {

042f5004aa911bdb1aeabbbc53755fcd98758039
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-15 11:02:15 +0100

    add style

--- src/components/Navigation/CartDetails.jsx
+++ src/components/Navigation/CartDetails.jsx
@@ -13,26 +13,29 @@ class CartDetails extends Component {
                 <div className="cart-details--item">
 
                     <table >
-                        {this.props.cart.map((item, index) => (
+                        {this.props.cart.length ?
+                            (this.props.cart.map((item, index) => (
 
-                            <tbody key={index}>
-                                <tr >
-                                    <td><img src={item.img} alt={item.alt} /></td>
+                                <tbody key={index}>
+                                    <tr >
+                                        <td><img src={item.img} alt={item.alt} /></td>
 
-                                    <td>
-                                        <p>Nazwa{item.name}</p>
-                                        <span> cena za jeden produkt{item.price}</span>
-                                        <span> Liczba Produktów{item.quantity}</span>
-                                    </td>
+                                        <td>
+                                            <p>Name{item.name}</p>
+                                            <span>
+                                                Price for one product{item.price}</span>
+                                            <span>
+                                                Amount of products{item.quantity}</span>
+                                        </td>
 
-                                    <td>
-                                        <span>cena łacznie {item.price * item.quantity}</span>
-                                        <span onClick={() => { this.handleRemove(item.id) }}>Usuń</span>
-                                    </td>
-                                </tr>
-                            </tbody>
+                                        <td>
+                                            <span>Expense {item.price * item.quantity}</span>
+                                            <span className="delete" onClick={() => { this.handleRemove(item.id) }}><i className="far fa-trash-alt"></i></span>
+                                        </td>
+                                    </tr>
+                                </tbody>
 
-                        ))}
+                            ))) : (<p className="cart-details--noProducts"> No products</p>)}
 
                     </table>
 
@@ -40,11 +43,11 @@ class CartDetails extends Component {
                 <div className="cart-details--summary">
                     <table>
                         <tr>
-                            <td>Przesyłka</td>
-                            <td>gratis</td>
+                            <td>Shipping</td>
+                            <td>Free</td>
                         </tr>
                         <tr>
-                            <td>Łączna kwota</td>
+                            <td>Total amount </td>
                             <td>{this.props.cartTotal} PLN</td>
                         </tr>
                     </table>

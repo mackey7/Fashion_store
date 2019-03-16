28ff5a42bdf15396f8ef3aff00d01db0c88d38af
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-16 13:56:54 +0100

    change style

--- src/components/Navigation/CartDetails.jsx
+++ src/components/Navigation/CartDetails.jsx
@@ -12,56 +12,74 @@ class CartDetails extends Component {
             <div id="item" className="cart-details" >
                 <p className="cart-details--title">My Cart</p>
                 <div className="cart-details--item">
-
-                    <table >
-                        {this.props.cart.length ?
-                            (this.props.cart.map((item, index) => (
-
-                                <tbody key={index}>
-                                    <tr >
+                    {this.props.cart.length ?
+                        (<table className="cart-details--table" >
+                            <thead>
+                                <tr>
+                                    <th> </th>
+                                    <th>
+                                        Name
+                                    </th>
+                                    <th>cena za sztuke </th>
+                                    <th> ilość</th>
+                                    <th>
+                                        Expense
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                {this.props.cart.map((item, index) => (
+                                    <tr key={index} >
                                         <td><img src={item.img} alt={item.alt} /></td>
 
                                         <td>
-                                            <p>Name{item.name}</p>
-                                            <span>
-                                                Price for one product{item.price}</span>
-                                            <span>
-                                                Amount of products{item.quantity}</span>
+                                            <p>{item.name}</p>
                                         </td>
 
                                         <td>
-                                            <span>Expense {item.price * item.quantity}</span>
+                                            {item.price}
+                                        </td>
+                                        <td>
+
+                                            {item.quantity}
+                                        </td>
+                                        <td>
+                                            <span> {item.price * item.quantity}{item.currency}</span>
                                             <span className="delete" onClick={() => { this.handleRemove(item.id) }}><i className="far fa-trash-alt"></i></span>
                                         </td>
                                     </tr>
-                                </tbody>
 
-                            ))) : (<p className="cart-details--noProducts"> No products</p>)}
+                                ))}
+                            </tbody></table>) : (<p className="cart-details--noProducts"> No products</p>)}
 
-                    </table>
 
-                </div>
-                <div className="cart-details--summary">
-                    <table>
-                        <tr>
-                            <td>Shipping</td>
-                            <td>Free</td>
-                        </tr>
-                        <tr>
-                            <td>Total amount </td>
-                            <td>{this.props.cartTotal} PLN</td>
-                        </tr>
-                    </table>
-                    <Link to="/cart">Cart</Link>
-                </div>
+
+
+                    <div className="cart-details--summary">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Shipping</td>
+                                    <td>Free</td>
+                                </tr>
+                                <tr>
+                                    <td>Total amount </td>
+                                    <td>{this.props.cartTotal} {this.props.products[0].currency}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <Link to="/cart">Cart</Link>
+                    </div>
+
+                </div >
             </div >
-
 
         )
     }
 }
 const mapStateToProps = state => {
     return {
+        products: state.products,
         cart: state.cart,
         cartTotal: state.cartTotal
 

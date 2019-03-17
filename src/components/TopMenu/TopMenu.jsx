eee09a73c93c71b25bd15691fa2e0530ebc2295b
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-17 23:19:48 +0100

    :fix style

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -10,44 +10,62 @@ class TopMenu extends Component {
         super();
         this.state = {
             search: false,
-            menuBar: true,
+            openMenu: false
         }
     }
 
-    handleClick = () => {
-        console.log("cli")
+
+    diplaySearch = () => {
         this.setState(state => ({ search: !state.search }))
     }
     diplayMenu = () => {
-        this.setState(state => ({ menuBar: !state.menuBar }))
+        this.setState(state => ({ openMenu: !state.openMenu }))
     }
-    handleChange = (e) => {
+    changeCurrency = (e) => {
         this.props.changeCurrency(e.target.value)
     }
 
     render() {
+        const width = window.innerWidth > 900;
+        const { openMenu } = this.state;
 
-        const menuStyle = {
-            display: this.state.menuBar ? 'flex' : 'none'
-        }
+
+        const nav = { display: openMenu ? " flex " : 'none' }
+
+
+
+        const search = this.state.search ? <SearchBar /> : null
+        const menuIcon = this.state.openMenu ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>
+
         return (
             <div className="topMenu" >
-
-                <a className="topMenu--telephone" href="tel:+88-23-4567"> custom care: 88-23-4567</a>
-                <div onClick={this.diplayMenu} className="menu-bar">
-                    {this.state.menuBar ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+                <div className="topMenu-row">
+                    <a className="topMenu--telephone" href="tel:+88-23-4567"> custom care: 88-23-4567</a>
+                    <div onClick={this.diplayMenu} className="menu-bar">
+                        {menuIcon}
+                    </div>
                 </div>
-
-                <div className="topMenu-account" style={menuStyle} >
-                    <select onChange={(e) => this.handleChange(e)}>
+                {width ? (<div className="topMenu--nav" >
+                    <select className="topMenu--nav__select" onChange={(e) => this.changeCurrency(e)}>
                         <option value="PLN">PLN</option>
                         <option value="USD">USD</option>
-
                     </select>
-                    <a href="#">  my account</a>
-                    <i onClick={this.handleClick} className="fas fa-search"></i>
-                    {this.state.search ? <SearchBar /> : null}
-                </div>
+                    <a className="topMenu--nav__account" href="#">  my account</a>
+                    <i onClick={this.diplaySearch} className=" searchIcon fas fa-search"></i>
+                    {search}
+                </div>)
+                    :
+                    (<div style={nav} className="topMenu--nav" >
+                        <select className="topMenu--nav__select" onChange={(e) => this.changeCurrency(e)}>
+                            <option value="PLN">PLN</option>
+                            <option value="USD">USD</option>
+                        </select>
+                        <a className="topMenu--nav__account" href="#">  my account</a>
+                        <i onClick={this.diplaySearch} className=" searchIcon fas fa-search"></i>
+                        {search}
+                    </div>)}
+
+
 
             </div >
         )
